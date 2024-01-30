1a0f639c9f21b89e023046891dd8e3ab0a900197
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2024-01-30 09:09:13 +0100

    milestone 1

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import axios from 'axios';
 
 export default {
     data() {
         return {
+            searchText:'',
+            movies: []
 
-        };
+        }
     },
     components: {
         AppHeader,
@@ -15,21 +18,59 @@ export default {
         AppFooter
     },  
     methods: {
+        search() {
+            axios
+              .get('https://api.themoviedb.org/3/search/movie', {
+                    params: { 
+                        api_key: 'e620333bf5f2944d736796855c58a280',
+                       query: this.searchText
+                    }
+                })
+                .then((response) => {
+                    console.log(response.data.results);
 
+                    this.movies = response.data.results;
+                })
+        }
     }
 }
 </script>
 
 <template>
-    <h1>
-        Mia App
-    </h1>
+    <header>
+        <input v-model="searchText" type="text" placeholder="Nome di un film...">
+        <button @click="search()">
+            Cerca
+        </button>
+    </header>
 
-    <AppHeader />
+    <main>
+        <div>
+            <ul>
+                <li v-for="(movie, i) in movies" :key="i">
+                    <div>
+                        Title: {{ movie.title }}
+                    </div>
+                    <div>
+                        Original title: {{ movie.original_title }}
+                    </div>
+                    <div>
+                        Language: {{ movie.original_language }}
+                    </div>
+                    <div>
+                        Voto: {{ movie.vote_average }}
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </main>
+
+    <!--<AppHeader />
 
     <AppMain />
 
-    <AppFooter />
+    <AppFooter />  -->
+    
 </template>
 
 <style lang="scss">
